f5fa270cc21e15b0c55ed09e1a10660afd63648a
hussien aldayyat <hussienaldayyat@example.com>
2026-07-22 21:37:50 +0300

    Add Firebase config and hosting setup

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA2lR01nABg3nu6fBDbigSqnrBsVH6XMyw",
+  authDomain: "khadra-workshop.firebaseapp.com",
+  projectId: "khadra-workshop",
+  storageBucket: "khadra-workshop.firebasestorage.app",
+  messagingSenderId: "96831234271",
+  appId: "1:96831234271:web:8fa1533fdfbf198f70096a",
+  measurementId: "G-54DHUBS5Q7"
+};
